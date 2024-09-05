94989567901603064e75daa6b3748f9e19f17a65
RezaMahendra21 <ejakganosec@gmail.com>
2024-09-05 20:20:36 +0700

    sistem pakar tanaman

---
+++ README.md
@@ -0,0 +1,44 @@
+# SISTEM PAKAR FORWARD CHAINING
+Sistem Pakar Diagnosa Penyakit Tanaman Sayur Menggunakan Metode Forward Chaining<br>
+Aplikasi ini di buat menggunakan PHP, MySQL dan Bootstrap 4, aplikasi ini bertujuan untuk
+mendeteksi diagnosa penyakit pada tanaman sayur sehingga dapat cara terbaik untuk penaggulangannya seperti,
+penyebab, solusi, dan cara mengobati juga teknik pengobatannya.
+
+<h2>Author By Reza Mahendra</h2>
+<br>
+<h3>Tampilan Aplikasi:</h3><br>
+<p>Halaman home</p>
+<img
+  src="/sample/home.png"
+  alt="Alt text"
+  title="Login"
+  style="display: inline-block; margin: 0 auto; width: 0 auto ;">
+
+  <p>Halaman diagnosa</p>
+<img
+  src="/sample/diagnosa.png"
+  alt="Alt text"
+  title="Home"
+  style="display: inline-block; margin: 0 auto; width: 0 auto ;">
+
+  <p>Halaman daftar penyakit</p>
+<img
+  src="/sample/daftar penyakit.png"
+  alt="Alt text"
+  title="Data Guru"
+  style="display: inline-block; margin: 0 auto; width: 0 auto ;">
+
+  <p>Halaman hasil diagnosa</p>
+<img
+  src="/sample/hasil diagnosa.png"
+  alt="Alt text"
+  title="Home"
+  style="display: inline-block; margin: 0 auto; width: 0 auto ;">
+
+  <p>Halaman profile</p>
+<img
+  src="/sample/profile.png"
+  alt="Alt text"
+  title="Data Guru"
+  style="display: inline-block; margin: 0 auto; width: 0 auto ;">
+
---
+++ login.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta charset="utf-8">
+  <meta name="description" content="SISTEM PAKAR DIAGNOSA PENYAKIT SAYUR ORGANIK">
+  <meta name="author" content="Reza Mahendra">
+  <meta name="keyword" content="SISTEM PAKAR, DIAGNOSA, PENYAKIT SAYUR ORGANIK">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>SISTEM PAKAR DIAGNOSA PENYAKIT SAYUR ORGANIK</title>
+
+  <!-- start: Css -->
+  <link rel="stylesheet" type="text/css" href="admin/asset/css/bootstrap.min.css">
+
+  <!-- plugins -->
+  <link rel="stylesheet" type="text/css" href="admin/asset/css/plugins/font-awesome.min.css"/>
+  <link rel="stylesheet" type="text/css" href="admin/css/plugins/simple-line-icons.css"/>
+  <link rel="stylesheet" type="text/css" href="admin/asset/css/plugins/animate.min.css"/>
+  <link rel="stylesheet" type="text/css" href="admin/asset/css/plugins/icheck/skins/flat/aero.css"/>
+  <link href="admin/asset/css/style.css" rel="stylesheet">
+  <!-- end: Css -->
+
+  <link rel="shortcut icon" href="asset/img/logomi.png">
+  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
+    </head>
+
+    <body id="mimin" class="dashboard form-signin-wrapper">
+    
+      <div class="container" style="margin-top: 100px;">
+
+        <form class="form-signin" action="proseslogin.php" method="POST">
+          <div class="panel periodic-login">
+              <span class="atomic-number"></span>
+              <div class="panel-body text-center">
+                  <p class="atomic-mass"><img src="lop.jpg" height="100" width="100" style="border-radius: 50px; border: 2px solid white; margin-bottom: 10px;" /></p>
+                  <p class="atomic-mass">PERINGATAN 404 </p>
+                   <p class="atomic-mass">Karena anda telah menggunakan code ini mohon tranfer ke</p><br />
+                   <p class="atomic-mass">dana : 0821-8193-6454 </p>
+              </div>
+                <div class="text-center" style="padding:5px;">
+                    <!-- <a href="forgotpass.html">Forgot Password </a>-->
+                    <p>Copyright &copy; 2024 <a href="rezamahendra21.github.io">Reza Mahendra</a></p>
+                </div>
+          </div>
+        </form>
+
+      </div>
+
+      <!-- end: Content -->
+      <!-- start: Javascript -->
+      <script src="admin/asset/js/jquery.min.js"></script>
+      <script src="admin/asset/js/jquery.ui.min.js"></script>
+      <script src="admin/asset/js/bootstrap.min.js"></script>
+
+      <script src="admin/asset/js/plugins/moment.min.js"></script>
+      <script src="admin/asset/js/plugins/icheck.min.js"></script>
+
+      <!-- custom -->
+      <script src="admin/asset/js/main.js"></script>
+      <script type="text/javascript">
+       $(document).ready(function(){
+         $('input').iCheck({
+          checkboxClass: 'icheckbox_flat-aero',
+          radioClass: 'iradio_flat-aero'
+        });
+       });
+     </script>
+     <!-- end: Javascript -->
+   </body>
+   </html>
